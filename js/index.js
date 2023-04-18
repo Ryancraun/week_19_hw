3ba5980f5948fec27ad8ed690d39bc0a3877286c
Ryancraun <117861991+Ryancraun@users.noreply.github.com>
2023-04-18 17:54:49 -0400

    homework 19

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+  const button = document.getElementById("myButton");
+button.addEventListener("mouseover", function() {
+  button.style.backgroundColor = "red";
+});
+button.addEventListener("mouseout", function() {
+  button.style.backgroundColor = "gray";
+});
